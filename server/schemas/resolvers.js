70cbae1f80804582a328bb8d3857fb8bfcb4c8e5
saltyposaune <ewalkertbone@gmail.com>
2021-11-14 15:58:32 -0700

    working on resolvers some more

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,21 +1,50 @@
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
                 const userData = await User.findOne({ _id: context.user._id })
-                .select('-__v -password')
-                .populate('books');
+                    .select('-__v -password')
+                    .populate('books');
 
                 return userData
             }
+
+            throw new AuthenticationError("Not logged in");
+            
         },
     },
 
     Mutation: {
+        addUser: async (parent, args) => {
+            try {
+                const user = await User.create(args);
 
+                const token = signToken(user);
+                return { token, user };
+            } catch (err) {
+                console.log(err);
+            }
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError("Incorrect credentials");
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError("Incorrect credentials");
+            }
+      
+            const token = signToken(user);
+            return { token, user };
+        },
     }
 }
 
