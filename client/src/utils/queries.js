ed5c104cde61fd4b227a04318696c350cfeac789
saltyposaune <ewalkertbone@gmail.com>
2021-11-19 21:31:21 -0700

    trying to deploy

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,20 +1,20 @@
 import gql from "graphql-tag";
 
 export const GET_ME = gql`
-    {
-        me {
-            _id
-            username
-            email
-            bookCount
-            savedBooks {
-                bookId
-                authors
-                title
-                description
-                image
-                link
-            }
-        }
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
     }
+  }
 `;
